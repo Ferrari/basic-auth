4113666deffcc33d94d738e432866923a5ef7763
Ferrari Lee <shiyung@gmail.com>
2016-09-27 12:59:06 +0800

    folk and rewrite to support jwt

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-/*!
- * basic-auth
- * Copyright(c) 2013 TJ Holowaychuk
- * Copyright(c) 2014 Jonathan Ong
- * Copyright(c) 2015 Douglas Christopher Wilson
- * MIT Licensed
- */
-
 'use strict'
+
+var jwt = require('jsonwebtoken')
 
 /**
  * Module exports.
@@ -16,36 +10,15 @@
 module.exports = auth
 
 /**
- * RegExp for basic auth credentials
- *
- * credentials = auth-scheme 1*SP token68
- * auth-scheme = "Basic" ; case insensitive
- * token68     = 1*( ALPHA / DIGIT / "-" / "." / "_" / "~" / "+" / "/" ) *"="
- * @private
- */
-
-var credentialsRegExp = /^ *(?:[Bb][Aa][Ss][Ii][Cc]) +([A-Za-z0-9\-\._~\+\/]+=*) *$/
-
-/**
- * RegExp for basic auth user/pass
- *
- * user-pass   = userid ":" password
- * userid      = *<TEXT excluding ":">
- * password    = *TEXT
- * @private
- */
-
-var userPassRegExp = /^([^:]*):(.*)$/
-
-/**
  * Parse the Authorization header field of a request.
  *
  * @param {object} req
- * @return {object} with .name and .pass
+ * @param {string} secret for JWT
+ * @return {Mixed} JWT payload
  * @public
  */
 
-function auth(req) {
+function auth(req, secret, opts) {
   if (!req) {
     throw new TypeError('argument req is required')
   }
@@ -58,30 +31,22 @@ function auth(req) {
   var header = getAuthorization(req.req || req)
 
   // parse header
-  var match = credentialsRegExp.exec(header || '')
+  var regexp
+  if (opts && opts.authSchema) {
+    regexp = new RegExp(opts.authSchema + '\\s+(\\S+)', 'i')
+  } else {
+    regexp = new RegExp('[JWT|jwt]+\\s+(\\S+)', 'i')
+  }
+  var match = regexp.exec(header || '')
 
   if (!match) {
     return
   }
 
-  // decode user pass
-  var userPass = userPassRegExp.exec(decodeBase64(match[1]))
-
-  if (!userPass) {
-    return
-  }
-
-  // return credentials object
-  return new Credentials(userPass[1], userPass[2])
-}
-
-/**
- * Decode base64 string.
- * @private
- */
-
-function decodeBase64(str) {
-  return new Buffer(str, 'base64').toString()
+  // return jwt encode payload
+  return (opts)
+    ? jwt.verify(match[1], secret, opts)
+    : jwt.verify(match[1], secret)
 }
 
 /**
@@ -95,14 +60,4 @@ function getAuthorization(req) {
   }
 
   return req.headers.authorization
-}
-
-/**
- * Object to represent user credentials.
- * @private
- */
-
-function Credentials(name, pass) {
-  this.name = name
-  this.pass = pass
 }
---
+++ test/jwt-parser.js
@@ -0,0 +1,52 @@
+var assert = require('assert');
+var auth = require('..');
+var jwt = require('jsonwebtoken');
+var secret = 'jwt-parser jwt sercert';
+
+function request(payload) {
+  return {
+    headers: {
+      authorization: (payload) ? 'JWT ' + jwt.sign(payload, secret) : ''
+    }
+  }
+}
+
+describe('auth(req)', function () {
+  describe('arguments', function () {
+    describe('req', function () {
+      it('should be required', function () {
+        assert.throws(auth, /argument req is required/)
+      })
+
+      it('should reject null', function () {
+        assert.throws(auth.bind(null, null), /argument req is required/)
+      })
+
+      it('should reject an object without headers', function () {
+        assert.throws(auth.bind(null, {}), /argument req is required/)
+      })
+    })
+  })
+
+  describe('with no Authorization field', function(){
+    it('should return null', function(){
+      var req = request();
+      assert(null == auth(req));
+    })
+  })
+
+  describe('with valid credentials', function(){
+    it('should return string payload', function () {
+      var req = request('basicZm9vOmJhcg')
+      var payload = auth(req, secret)
+      assert.equal(payload, 'basicZm9vOmJhcg')
+    })
+
+    it('should return object payload', function(){
+      var req = request({name: 'foo', pass: 'bar'});
+      var payload = auth(req, secret);
+      assert.equal(payload.name, 'foo');
+      assert.equal(payload.pass, 'bar');
+    })
+  })
+})
